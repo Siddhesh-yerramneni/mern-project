7aead9bac9cd08589bbb84ba9830f4bcdf794de8
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-07-15 09:00:17 -0400

    Created a few pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+import About from './pages/About'; // Ensure this is the correct path
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>APP</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
