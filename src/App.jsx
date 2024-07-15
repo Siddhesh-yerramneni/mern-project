21868be0a055dfc67aa4a994f74c1647a72f46df
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-07-15 10:28:59 -0400

    Created a header and added the links!!

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignOut from './pages/SignOut';
-import Profile from './pages/Profile';
-import About from './pages/About'; // Ensure this is the correct path
+import SignOut from './pages/SignOut'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
