0f331ef49176ebd588811476f7b36e0cf0a365f9
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-07-25 10:56:50 -0400

    Created an user update api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updatedUser } from '../controllers/user.controller.js';
+import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test)
+
+router.post('/update/:id', verifyUser, updatedUser)
 export default router;
