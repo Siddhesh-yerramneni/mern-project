f35037be35a7304f0fcad7cf0523df185ee60437
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-07-27 22:05:55 -0400

    Created user delete route and functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -30,3 +30,14 @@ export const updatedUser = async(req,res,next) => {
         next(error);
     }
 };
+
+export const deleteUser = async(req,res,next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "Action not allowed!!"));
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json('User is deleted!');
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updatedUser } from '../controllers/user.controller.js';
+import { test, updatedUser, deleteUser } from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,4 +7,7 @@ const router = express.Router();
 router.get('/test', test)
 
 router.post('/update/:id', verifyUser, updatedUser)
+
+router.delete('/delete/:id',verifyUser, deleteUser)
+
 export default router;
