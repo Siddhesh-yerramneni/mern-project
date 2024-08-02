5e9c5a0de3286698ffd8ad9fed1b063aae98be82
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-08-01 20:36:37 -0400

    Corrected an error

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ export default function CreateListing() {
   return (
       <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7' >Create Listing</h1>
-        <form className='flex flex-xol sm:flex-row gap-4'>
+        <form className='flex flex-col sm:flex-row gap-4'>
             <div className='flex flex-col gap-4 flex-1'>
                 <input type="text" placeholder='Name' className='border p-3 rounded-lg' id='name' maxLength='20' required />
                 <textarea type="text" placeholder='Description' className='border p-3 rounded-lg' id='description' maxLength='20' required />
